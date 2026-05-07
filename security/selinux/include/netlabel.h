c14779a9fe01bc8cd7dacebebbef1920eb08507d
Kayasse <98520453+Kayasse@users.noreply.github.com>
2026-05-07 23:12:53 +0700

    Fix: Remove return statements from void functions in netlabel.h

--- security/selinux/include/netlabel.h
+++ security/selinux/include/netlabel.h
@@ -73,7 +73,6 @@ int selinux_netlbl_socket_connect_locked(struct sock *sk,
 #else
 static inline void selinux_netlbl_cache_invalidate(void)
 {
-	return;
 }
 
 static inline void selinux_netlbl_err(struct sk_buff *skb,
@@ -81,87 +80,82 @@ static inline void selinux_netlbl_err(struct sk_buff *skb,
 				      int error,
 				      int gateway)
 {
-	return;
 }
 
 static inline void selinux_netlbl_sk_security_free(
-					       struct sk_security_struct *sksec)
+				       struct sk_security_struct *sksec)
 {
-	return;
 }
 
 static inline void selinux_netlbl_sk_security_reset(
-					       struct sk_security_struct *sksec)
+				       struct sk_security_struct *sksec)
 {
-	return;
 }
 
 static inline int selinux_netlbl_skbuff_getsid(struct sk_buff *skb,
-					       u16 family,
-					       u32 *type,
-					       u32 *sid)
+				       u16 family,
+				       u32 *type,
+				       u32 *sid)
 {
 	*type = NETLBL_NLTYPE_NONE;
 	*sid = SECSID_NULL;
 	return 0;
 }
 static inline int selinux_netlbl_skbuff_setsid(struct sk_buff *skb,
-					       u16 family,
-					       u32 sid)
+				       u16 family,
+				       u32 sid)
 {
 	return 0;
 }
 
 static inline int selinux_netlbl_conn_setsid(struct sock *sk,
-					     struct sockaddr *addr)
+				     struct sockaddr *addr)
 {
 	return 0;
 }
 
 static inline int selinux_netlbl_sctp_assoc_request(struct sctp_endpoint *ep,
-						    struct sk_buff *skb)
+					    struct sk_buff *skb)
 {
 	return 0;
 }
 static inline int selinux_netlbl_inet_conn_request(struct request_sock *req,
-						   u16 family)
+					   u16 family)
 {
 	return 0;
 }
 static inline void selinux_netlbl_inet_csk_clone(struct sock *sk, u16 family)
 {
-	return;
 }
 static inline void selinux_netlbl_sctp_sk_clone(struct sock *sk,
-						struct sock *newsk)
+					struct sock *newsk)
 {
-	return;
 }
 static inline int selinux_netlbl_socket_post_create(struct sock *sk,
-						    u16 family)
+					    u16 family)
 {
 	return 0;
 }
 static inline int selinux_netlbl_sock_rcv_skb(struct sk_security_struct *sksec,
-					      struct sk_buff *skb,
-					      u16 family,
-					      struct common_audit_data *ad)
+				      struct sk_buff *skb,
+				      u16 family,
+				      struct common_audit_data *ad)
 {
 	return 0;
 }
 static inline int selinux_netlbl_socket_setsockopt(struct socket *sock,
-						   int level,
-						   int optname)
+					   int level,
+					   int optname)
 {
 	return 0;
 }
 static inline int selinux_netlbl_socket_connect(struct sock *sk,
-						struct sockaddr *addr)
+					struct sockaddr *addr)
 {
 	return 0;
 }
 static inline int selinux_netlbl_socket_connect_locked(struct sock *sk,
-						       struct sockaddr *addr)
+					       struct sockaddr *addr)
 {
 	return 0;
 }
